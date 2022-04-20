70d53956f1ef232b268ade58856b3c7e3052cc8a
eui <wlsun0324@gmail.com>
2022-04-21 00:26:50 +0900

    Fix : 회원가입 분리

--- controllers/userController.js
+++ controllers/userController.js
@@ -20,25 +20,8 @@ const signup = async (req, res, next) => {
 
 }
 
-const signin = async (req, res, next) => {
-
-    try {
-        const { email, password } = req.body;
-        
-        const accessToken = await userService.createToken(email, password, res);
-
-        res.status(200).json({accessToken : accessToken});
-    } catch (error) {
-        next(error);
-        await prisma.$disconnect();
-    } finally {
-        await prisma.$disconnect();
-    }
-
-}
-
 const error = (err, req, res, next) => {
     console.error(err);
 }
 
-module.exports = { error, signup, signin }
+module.exports = { error, signup }
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -4,7 +4,6 @@ const router = express.Router();
 const userController = require("../controllers/userController");
 
 router.post("/signup", userController.signup);
-router.post("/signin", userController.signin);
 router.use("/", userController.error);
 
 module.exports = router;
