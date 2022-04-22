6f9f91e23c9ec5b563e677238ba678630b69a3f6
eui <wlsun0324@gmail.com>
2022-04-23 03:32:28 +0900

    Add : 숙소등록 api 부분 코드 추가

---
+++ controllers/accommodationsController.js
@@ -0,0 +1,28 @@
+const userService = require("../services/userService");
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
+
+const registration = async (req, res, next) => {
+
+    try {
+        const { accessToken, userId } = req.headers;
+        const { name, email, password } = req.body;
+        
+        await userService.createUser(name, email, password, res);
+
+        res.status(201).json({message : `create user success`});
+    } catch (error) {
+        next(error);
+        await prisma.$disconnect();
+    } finally {
+        await prisma.$disconnect();
+    }
+
+}
+
+const error = (err, req, res, next) => {
+    console.error(err);
+}
+
+module.exports = { error, registration }
