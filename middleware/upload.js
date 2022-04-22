2c9213fe78ebec54cd2745fb05c580e7adc133a5
eui <wlsun0324@gmail.com>
2022-04-22 18:15:52 +0900

    Add : 이미지 s3 업로드 기능 추가

---
+++ middleware/upload.js
@@ -0,0 +1,65 @@
+const AWS = require("aws-sdk");
+const multer = require("multer");
+const multers3 = require("multer-s3");
+const path = require("path");
+
+const s3 = new AWS.S3({
+    region : process.env.AWS_REGION,
+    accessKeyId : process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey : process.env.AWS_SECRET_ACCESS_KEY
+});
+
+let nowDate = new Date();
+let directory = `${nowDate.getFullYear()}_${nowDate.getMonth()+1}_${nowDate.getDate()}`;
+const allowFileExtension = [".jpg",".jpeg"];
+const limitsize = 3 * 1024 * 1024;
+const limitCount = 10;
+
+let fileErr = new Error("file invalid type");
+fileErr.code = "INVALID_TYPE";
+
+/*
+    one -> upload.single("key_name")
+    many -> upload.array("key_name")
+    catch error : INVALID_TYPE, Too many files(LIMIT_FILE_COUNT), File too large(LIMIT_FILE_SIZE)
+    upload -> next middleware -> req.file or req.files
+*/
+const upload = multer({
+    storage: multers3({
+        s3: s3,
+        bucket: process.env.AWS_BUCKET_NAME,
+        acl: 'public-read',
+        contentType: multers3.AUTO_CONTENT_TYPE,
+        key: (req, file, cb) => {
+
+            if(!allowFileExtension.includes(path.extname(file.originalname.toLocaleLowerCase()))) {                
+                return cb(fileErr);
+            }
+            cb(null, `${directory}/${Date.now()}_${file.originalname}`);
+        },
+    }),
+    limits: {
+        fileSize: limitsize,
+        files: limitCount
+    },
+});
+const uploader = upload.array("image");
+
+const uploadHandle = async (req, res, next) => {
+
+    try {
+        uploader(req, res, (err) => {
+            if(err) {
+                console.error(err);
+                return res.status(400).json({message : err.code});
+            }
+            next();
+        });
+        
+    } catch (error) {
+        next(error);
+    }
+
+}
+
+module.exports = { uploadHandle }
