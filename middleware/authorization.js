23dfd36928d60f2504cb59a295cfdf9dbc6452db
eui <wlsun0324@gmail.com>
2022-04-22 17:01:39 +0900

    Fix : jwt 토큰 만료시간 수정 및 토큰검증 기능분리

--- middleware/authorization.js
+++ middleware/authorization.js
@@ -7,20 +7,14 @@ const auth = async (req, res, next) => {
         const { accessToken } = req.body;
         
         jwt.verify(accessToken, process.env.SECRET_KEY, 
+
             (err, decode) => {
 
                 if(!!err) {
                     console.log("jwt Error : " + err.message);
                     errService.errorHandler(409, "INVALID_TOKEN", res);
-
-                }else {
-                    let nowDate = new Date(parseInt(decode.exp) * 1000);
-                    let expDate = new Date();
-                    let newAccessToken;
-                    
-                    (nowDate-expDate) < 550000 ? 
-                        newAccessToken = jwt.sign({userId : decode.userId}, process.env.SECRET_KEY, {expiresIn: "30m"}) 
-                        : newAccessToken = accessToken;
+                }else {                    
+                    const newAccessToken = updateToken(decode, accessToken);
 
                     req.headers.accessToken = newAccessToken;
                     req.headers.userId = decode.userId;
@@ -32,6 +26,18 @@ const auth = async (req, res, next) => {
         next(error);        
     }
 
+}
+
+const updateToken = (decode, accessToken) => {
+    let nowDate = new Date();
+    let expDate = new Date(parseInt(decode.exp) * 1000);
+    let newAccessToken;
+    
+    (expDate-nowDate) < 60 * 60 * 1000 ? 
+        newAccessToken = jwt.sign({userId : decode.userId}, process.env.SECRET_KEY, {expiresIn: "3h"})
+        : newAccessToken = accessToken;
+
+    return newAccessToken;
 }
 
 const error = (err, req, res, next) => {
